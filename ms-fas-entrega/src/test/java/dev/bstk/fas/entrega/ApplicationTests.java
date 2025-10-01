3ed7bc813c19fb21312e0ca3ee6473450f781a3f
Bruno Luz <bsilva.se@gmail.com>
2025-10-01 10:30:17 -0300

    main - Criando componentes principais do fluxo de entrega de pedidos

---
+++ ms-fas-entrega/src/test/java/dev/bstk/fas/entrega/ApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.bstk.fas.entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertDoesNotThrow(() -> {});
+  }
+}
---
+++ ms-fas-inventario/src/test/java/dev/bstk/fas/inventario/ApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.bstk.fas.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertDoesNotThrow(() -> {});
+  }
+}
